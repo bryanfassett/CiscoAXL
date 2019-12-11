c5e60e8be563650c69a68937824d2aefa270ef41
Bryan <bryanfassett@gmail.com>
2019-12-10 20:57:35 -0600

    Create Device Pools

--- region.py
+++ region.py
@@ -8,9 +8,9 @@ from urllib3.exceptions import InsecureRequestWarning
 # Create a Dictionary of all regions to create a relationship with the new region
 region_matrix = {
     # Region Name:  Region uuid
-    'CLx_R': '3a2eb8c1-7f22-dd9e-dfee-ff6179034c57',
-    'SBC_CLx_R':'648e7571-a570-59ae-305c-ca3f1ab73776',
-    'BROADCAST_CLx_R': 'e6efbe6c-5038-1ab8-a3ff-8f18188612d9'
+    'CLx_R': 'e80815d6-da74-033c-6294-d6fce4fc7da9',
+    'SBC_CLx_R':'138bb056-38bf-956a-7a54-e1d122521e6a',
+    'BROADCAST_CLx_R': '09835766-1838-9229-faff-bdda583af704'
 }
 
 disable_warnings(InsecureRequestWarning) # Disable warning output due to invalid certificate
