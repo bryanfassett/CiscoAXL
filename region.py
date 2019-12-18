0b764c410f621641c5ddf27dfc17e118fdedee8a
kllyhlls <kllyhlls@gmail.com>
2019-12-18 15:30:41 -0600

    Updated Region and StageLab files

--- region.py
+++ region.py
@@ -17,33 +17,61 @@ from stagelab import region_matrix
 disable_warnings(InsecureRequestWarning) # Disable warning output due to invalid certificate
 client, service, history = open_connection() # Open connection using connect.py
 
-# Get name of the new region
-print("Enter new region name:")
-region_name= input() # wait for input
-print (f"\nCreating new region named {region_name}...")
-
-# Create new region and store uuid
+# Attempting to pull the uuids for each Region in the base Region List
 try:
-    # Create the new region
-    resp = service.addRegion(region={"name": region_name})
+    # Creating the base Region List
+    base_region_list = ['CLX_R','SBC_CLx_R','BROADCAST_CLx_R']
+    # Creating an empty region_matrix to hold the Region name and uuid results, maybe not needed since I can create it during the for loop
+    region_matrix = {}
 
-    # Store the returned uuid for later
-    region_uuid = resp['return']
-    region_uuid = region_uuid.strip("{}").lower()
-    print(f"New region uuid: {region_uuid}")
+    for region_name in base_region_list:
+        region_result = service.getRegion(region_name)
+        #NO IDEA... I don't know the syntax of .getRegion and don't know what is returned
+        region_matrix.update({name}:uuid)
 
-    # Loop through dictionary and run insert statement for each key (informix DB can't do values listing...)
-    for region in region_matrix:
-        sql_stmt = '''
-            INSERT INTO regionmatrix (fkregion_a, fkregion_b, videobandwidth, fkcodeclist, immersivebandwidth, audiobandwidth)
-            VALUES ('{new_uuid}', '{target_uuid}', 384, '22910f2b-51ab-4a46-b606-604a28558568', -2, 64)
-        '''.format(
-                new_uuid = region_uuid,
-                target_uuid = region_matrix[region]
-            )
-        
-        resp = service.executeSQLUpdate(sql_stmt)
+        #if region_result:
+        #    print region_result
+        #else:
+        #    print ("No results")
 
-    print('Region successfully created.')
 except Fault as err:
     print(f'Error Inserting Region: {err}')
+
+
+
+# EVERYTHING BELOW COMMENTED OUT TO TROUBLESHOOT THE .getRegion
+
+
+# Get name of the new region
+# print("Enter new region name:")
+# region_name= input() # wait for input
+# print (f"\nCreating new region named {region_name}...")
+
+
+
+
+# # Create new region and store uuid
+# try:
+#     # Create the new region
+#     resp = service.addRegion(region={"name": region_name})
+
+#     # Store the returned uuid for later
+#     region_uuid = resp['return']
+#     region_uuid = region_uuid.strip("{}").lower()
+#     print(f"New region uuid: {region_uuid}")
+
+#     # Loop through dictionary and run insert statement for each key (informix DB can't do values listing...)
+#     for region in region_matrix:
+#         sql_stmt = '''
+#             INSERT INTO regionmatrix (fkregion_a, fkregion_b, videobandwidth, fkcodeclist, immersivebandwidth, audiobandwidth)
+#             VALUES ('{new_uuid}', '{target_uuid}', 384, '22910f2b-51ab-4a46-b606-604a28558568', -2, 64)
+#         '''.format(
+#                 new_uuid = region_uuid,
+#                 target_uuid = region_matrix[region]
+#             )
+        
+#         resp = service.executeSQLUpdate(sql_stmt)
+
+#     print('Region successfully created.')
+# except Fault as err:
+#     print(f'Error Inserting Region: {err}')
--- stagelab.py
+++ stagelab.py
@@ -1,7 +1,6 @@
 from connect import open_connection, show_history
 from zeep import Client
 from zeep.exceptions import Fault
-from zeep.plugins import HistoryPlugin
 from urllib3 import disable_warnings
 from urllib3.exceptions import InsecureRequestWarning
 
@@ -10,18 +9,30 @@ from urllib3.exceptions import InsecureRequestWarning
 disable_warnings(InsecureRequestWarning) # Disable warning output due to invalid certificate
 client, service, history = open_connection() # Open connection using connect.py
 
+
+
 def stage_regions():
     # Create Blank Region Dictionary
-    region_matrix = {}
-    # Create initial hardcoded Regions List to test with
-    region_list = ['CLX_R','SBC_CLx_R','BROADCAST_CLx_R']
+    # region_matrix = {}
+    try:
+        # Create initial hardcoded Regions List to test with
+        base_region_list = ['CLX_R','SBC_CLx_R','BROADCAST_CLx_R']
+        
+        # Create the new regions from hardcoded region list
+        for region_name in base_region_list:
+            print (f"\nCreating new region named {region_name}...")
+            service.addRegion(region={"name": region_name})
+            # Store the returned uuids not needed.
+            # Will query when needed at runtime in each script.
+            # region_uuid = resp['return']
+            # region_uuid = region_uuid.strip("{}").lower()
+            # print(f"New region uuid: {region_uuid}")
+            # region_matrix.update({region_name:region_uuid})
+            print (f"Region {region_name} successfully created.")
+        print(f"\nDONE!\n")
 
-    # Create the new regions from hardcoded region list
-    for region_name in region_list:
-        print (f"Creating new region named {region_name}...")
-        resp = service.addRegion(region={"name": region_name})
-        # Store the returned uuids for later
-        region_uuid = resp['return']
-        region_uuid = region_uuid.strip("{}").lower()
-        print(f"New region uuid: {region_uuid}")
-        region_matrix.update({region_name:region_uuid})
+
+    except Fault as err:
+        print(f'Error Inserting Region: {err}')
+
+stage_regions()
