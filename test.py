6048ec24819c8bbd27b620afcdffa0389fb41a28
Bryan <bryanfassett@gmail.com>
2020-01-03 13:50:41 -0600

    Examples

--- test.py
+++ test.py
@@ -2,9 +2,12 @@ import CiscoAXL
 from CiscoAXL import Site
 
 print("Creating site")
-newsite = Site("TX999")
+
+
+newsite = Site("TX999") # created an instance of the class
 newsite.TZ = "CMLocal"
 newsite.Carrier = "ATT"
 newsite.CallManagerGroup = "2A"
 newsite.CAC = 4444
 newsite.Build()
+
