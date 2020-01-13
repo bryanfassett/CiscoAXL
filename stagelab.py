64ed25a37ea096575ae2e7bfd8611663b145edd2
kllyhlls <kllyhlls@gmail.com>
2020-01-13 00:47:32 -0600

    updated Build CSS to work with the Re-Re-Fixed Build CSS

--- stagelab.py
+++ stagelab.py
@@ -151,15 +151,14 @@ def stageCSS(AbbrevCluster):
         conn = AxlConnection(WSDL)
         CssDict = {f"{AbbrevCluster}_Trans_CSS":f"Cluster TransPattern CSS",f"{AbbrevCluster}_Inbound_CSS":f"Cluster Inbound Access",f"{AbbrevCluster}_Internal_CSS":f"Cluster Internal Only CSS",f"{AbbrevCluster}_Local_CSS":f"Cluster Local Dialing CSS",f"{AbbrevCluster}_LongDistance_CSS":f"Cluster Long Distance Dialing CSS",f"{AbbrevCluster}_International_CSS":f"Cluster International Dialing CSS"}
         MemberListofList = [
-            [
-                [f"{AbbrevCluster}_Trans_PT",f"{AbbrevCluster}_Outbound_PT",f"E911_{AbbrevCluster}_Hunt_PT",f"{AbbrevCluster}_CMService_PT"],
-                [f"{AbbrevCluster}_DN_PT",f"E911_{AbbrevCluster}_Hunt_PT"],
-                [f"{AbbrevCluster}_Trans_PT",f"{AbbrevCluster}_Outbound_PT",f"{AbbrevCluster}_CMService_PT"],
-                [f"{AbbrevCluster}_DN_PT"],
-                [f"{AbbrevCluster}_DN_PT",f"{AbbrevCluster}_Outbound_PT"],
-                [f"{AbbrevCluster}_DN_PT",f"{AbbrevCluster}_Outbound_PT"]
-            ]
+            [f"{AbbrevCluster}_Trans_PT",f"{AbbrevCluster}_Outbound_PT",f"E911_{AbbrevCluster}_Hunt_PT",f"{AbbrevCluster}_CMService_PT"],
+            [f"{AbbrevCluster}_DN_PT",f"E911_{AbbrevCluster}_Hunt_PT"],
+            [f"{AbbrevCluster}_Trans_PT",f"{AbbrevCluster}_Outbound_PT"],
+            [f"{AbbrevCluster}_DN_PT"],
+            [f"{AbbrevCluster}_DN_PT",f"{AbbrevCluster}_Outbound_PT"],
+            [f"{AbbrevCluster}_DN_PT",f"{AbbrevCluster}_Outbound_PT"]
         ]
+
         if conn.Open():
             result, details = BuildCSS(conn.Service, CssDict, MemberListofList)
             if not result:
